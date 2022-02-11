c06df52eb6375de66475e380501c60b2db644cfb
mkubera <rainteller@tutanota.de>
2022-02-11 21:41:42 +0100

    Add counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,55 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react"; // React Hook do stanu (model)
 
-function App() {
+const App = () => {
+  // Model
+  const INIT_COUNTER = 10;
+  const [counter, setCounter] = useState(INIT_COUNTER);
+  const [appTitle, setAppTitle] = useState("Counter App");
+
+  // Update
+  const incrementCounter = (state) => state + 1;
+  const decrementCounter = (state) => state - 1;
+  const resetCounter = () => INIT_COUNTER;
+
+  // Events
+  const incrementCounterEvent = (e) => setCounter(incrementCounter);
+  const decrementCounterEvent = (e) => setCounter(decrementCounter);
+  const resetCounterEvent = (e) => setCounter(resetCounter);
+  const setAppTitleEvent = (e) => {
+    e.preventDefault();
+    const { title } = e.currentTarget.elements;
+    setAppTitle((state) => title.value);
+  };
+
+  // View
   return (
     <div className="App">
-      <header className="App-header">{/* ... */}</header>
+      <header className="App-header">
+        {/* <p>2 + 2 = {2 + 2}</p> */}
+        <h1>{`${appTitle.toUpperCase()}`}</h1>
+        <form onSubmit={setAppTitleEvent}>
+          <input type="text" name="title" defaultValue={appTitle} />
+          <button>Change title</button>
+        </form>
+        <div id="container">
+          <h2>Counter</h2>
+          <span id="counter">{counter}</span>
+          <div>
+            <button id="plus" onClick={incrementCounterEvent}>
+              +
+            </button>
+            <button id="minus" onClick={decrementCounterEvent}>
+              -
+            </button>
+          </div>
+          <button id="reset" onClick={resetCounterEvent}>
+            Reset
+          </button>
+        </div>
+      </header>
     </div>
   );
-}
+};
 
 export default App;
